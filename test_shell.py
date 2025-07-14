552afb0b97f8fe406372bbe23411a972eb5b34c2
youngchul.hwang <sofe072@gmail.com>
2025-07-14 15:18:27 +0900

    [add] test_help_text_valid 추가

--- test_shell.py
+++ test_shell.py
@@ -21,5 +21,19 @@ def test_help_call(mocker:MockerFixture):
 
     mk.help.assert_called_once()
 
-def test_help_text_valid():
-    pass
+def test_help_text_valid(capsys):
+    shell = Shell()
+    ret = shell.help()
+    captured = capsys.readouterr()
+
+    assert captured.out.strip() == '''제작자: 배성수 팀장, 연진혁, 이정은, 이찬욱, 임창근, 정구환, 이근우
+명령어 사용 법 : 
+1. read : read + idx
+2. write : write + idx + contents
+3. exit : exit
+4. fullwrite : fullwrite + contents
+5. fullread : fullread
+6. 1_FullWriteAndReadCompare : 1_ 혹은 1_FullWriteAndReadCompare 입력
+7. 2_PartialLBAWrite : 2_ 혹은 2_PartialLBAWrite 입력
+8. 3_WriteReadAging : 3_ 혹은 3_WriteReadAging 입력
+9. 그 외 명령어 입력 시, INVALID COMMAND 가 출력 됩니다.'''
