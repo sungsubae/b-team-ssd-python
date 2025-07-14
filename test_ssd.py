7cee6a6103346e87bd67794c6b4cae5dd853de38
lkwoo <dlrmsdn135@naver.com>
2025-07-14 15:32:10 +0900

    feature: ssd write
    add write func and test code

---
+++ test_ssd.py
@@ -0,0 +1,35 @@
+import pytest
+from pytest_mock import MockerFixture
+from ssd import SSD
+
+
+@pytest.fixture
+def reset_ssd():
+    with open('ssd_nand.txt', 'w', encoding='utf-8') as f:
+        for i in range(100):
+            f.write(f'{i:02d} 0x00000000\n')
+
+    with open('ssd_output.txt', 'w', encoding='utf-8') as f:
+        f.write('')
+
+
+def test_ssd_write(reset_ssd):
+    ssd = SSD()
+
+    ssd.write(-1, '0x00113456')
+    output = ssd.read_output()
+    assert output == "ERROR"
+
+    ssd.write(2, '0x00113456')
+    contents = ssd.read_all()
+    output = ssd.read_output()
+    assert contents[2] == f"02 0x00113456\n" and output == ""
+
+    ssd.write(987, '0x00113456')
+    output = ssd.read_output()
+    assert output == "ERROR"
+
+    ssd.write(99, '0xFF34FF33')
+    contents = ssd.read_all()
+    output = ssd.read_output()
+    assert contents[99] == f"99 0xFF34FF33\n" and output == ""
