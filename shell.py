216d531251dd5871e5ec31a63069f06ff0c50943
jinhyeok <wlsgudl94@naver.com>
2025-07-14 15:01:27 +0900

    shell write 기본 기능 및 test 추가 (red)

--- shell.py
+++ shell.py
@@ -4,12 +4,4 @@ class Shell:
         self.data = [0] * size  # 내부 스토리지
 
     def ssd_write(self, line):
-        tokens = line.strip().split()
-        if tokens[0] == "write":
-            try:
-                lba = int(tokens[1])
-                value = int(tokens[2], 16) # 16진수
-                self.data[lba] = value
-                return "[Write] Done"
-            except Exception:
-                return "Usage: write <LBA> <VALUE>"
+        return 0
