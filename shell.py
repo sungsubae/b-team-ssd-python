b102122fff558f9243da75777a472a79e0b2b42f
youngchul.hwang <sofe072@gmail.com>
2025-07-14 14:29:50 +0900

    shell에서 help 입력 시, 프로그램 사용법 출력 될 수 있도록 기능 추가 하였습니다.

--- shell.py
+++ shell.py
@@ -1,6 +1,21 @@
 class Shell:
+
     def read(self, index: int):
         if 0 <= index <= 99:
             print(f"[Read] LBA {index:02d} : 0xAAAABBBB")
         else:
             print("[Read] ERROR")
+
+    def help(self):
+        print('제작자: 배성수 팀장, 연진혁, 이정은, 이찬욱, 임창근, 정구환, 이근우')
+        print('명령어 사용 법 : ')
+        print('1. read : read + idx')
+        print('2. write : write + idx + contents')
+        print('3. exit : exit')
+        print('4. fullwrite : fullwrite + contents')
+        print('5. fullread : fullread')
+        print("6. 1_FullWriteAndReadCompare : 1_ 혹은 1_FullWriteAndReadCompare 입력")
+        print("7. 2_PartialLBAWrite : 2_ 혹은 2_PartialLBAWrite 입력")
+        print("8. 3_WriteReadAging : 3_ 혹은 3_WriteReadAging 입력")
+        print("9. 그 외 명령어 입력 시, INVALID COMMAND 가 출력 됩니다.")
+
