9f7348f60910e19bd9816b35d28ef0bc3d828e34
youngchul.hwang <sofe072@gmail.com>
2025-07-15 13:04:39 +0900

    [fix]full_read test ssd reset 추개

--- test_shell.py
+++ test_shell.py
@@ -174,6 +174,8 @@ def test_fullread_call(mocker:MockerFixture):
 
 def test_fullread_valid(mocker:MockerFixture, capsys):
     shell = Shell()
+    shell._ssd_reset()
+
     shell.fullread()
     captured = capsys.readouterr()
     assert captured.out.strip() == '''[Full Read]
