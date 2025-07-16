5b22d8f4d769fa480cdbc58222f1b8bc05da7ff8
ijsilver <dlwjddms0102@hanmail.net>
2025-07-16 10:25:15 +0900

    [feature] Buffer 클래스 및 인터페이스 추가

---
+++ buffer.py
@@ -0,0 +1,12 @@
+class Buffer:
+    def __init__(self):
+        pass
+
+    def read(self, lba: int):
+        pass
+
+    def write(self, lba: int, value: str):
+        pass
+
+    def flush(self):
+        pass
