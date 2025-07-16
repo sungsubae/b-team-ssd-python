24218f8a74a4155c1c495a543bc7b50d373fa405
lkwoo <dlrmsdn135@naver.com>
2025-07-16 12:42:35 +0900

    add test: example 2
    명세서의 두 번째 예제 적용했습니다.

--- test_command_buffer.py
+++ test_command_buffer.py
@@ -4,6 +4,8 @@ from unittest.mock import call, patch, mock_open
 import builtins
 import os
 from shell import Shell, main
+from ssd import SSD
+
 
 buffer_path = r'.\buffer'
 
@@ -32,7 +34,8 @@ def init_buffer():
         write_buffer(i, 'empty')
 
 
-def test_command_buffer_flush(capsys, mocker: MockerFixture, init_buffer):
+def test_command_buffer_flush(init_buffer):
+    # command buffer 적용되면 아래 주석 해제 후 TEST
     # write_buffer(1, 'R_10')
     # write_buffer(2, 'W_10_0x12341234')
     # do flush here
@@ -43,13 +46,12 @@ def test_command_buffer_flush(capsys, mocker: MockerFixture, init_buffer):
     assert expected_files.issubset(actual_files)
 
 
-# 1_W_20_0xABCDABCD , 2_W_20_0x12341234 , 3_E_20_1
 def test_command_buffer_example_1(mocker: MockerFixture, init_buffer):
     shell_write_mock = mocker.patch('shell.Shell._write')
     shell_erase_mock = mocker.patch('shell.Shell._erase')
 
-    shell = Shell()
     # command buffer 적용되면 아래 주석 해제 후 TEST
+    # shell = Shell()
     # shell.write(20, "0xABCDABCD")
     # shell.write(20, "0x12341234")
     # shell.erase(20, 1)
@@ -61,3 +63,22 @@ def test_command_buffer_example_1(mocker: MockerFixture, init_buffer):
     actual_files = set(os.listdir(buffer_path))
 
     assert expected_files.issubset(actual_files)
+
+
+def test_command_buffer_example_2(mocker: MockerFixture, init_buffer):
+    write_buffer(1, 'W_50_0xAAAABBBB')
+    write_buffer(2, 'W_20_0xABABCCCC')
+
+    shell_read_mock = mocker.patch('shell.Shell._read')
+
+    # command buffer 적용되면 아래 주석 해제 후 TEST
+    # shell = Shell()
+    # shell.read(50)
+
+    shell_read_mock.assert_not_called()
+
+    expected_files = {'1_W_50_0xAAAABBBB', '2_W_20_0xABABCCCC', '3_empty', '4_empty', '5_empty'}
+    actual_files = set(os.listdir(buffer_path))
+
+    assert expected_files.issubset(actual_files)
+    assert SSD().read_output().strip() == '0xAAAABBBB'
