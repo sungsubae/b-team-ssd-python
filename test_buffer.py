6126eca36fe588f43c1f0b61ba02fe5858ac4de5
ijsilver <dlwjddms0102@hanmail.net>
2025-07-16 12:58:19 +0900

    [fix] 오타 수정

--- test_buffer.py
+++ test_buffer.py
@@ -6,6 +6,6 @@ def test_buffer_write_and_read():
     buffer.make_init_buffer()
 
     buffer.write('E', 5, size=5)
-    assert buffer.read(5) == '0x0x00000000'
-    buffer.write('W', 5, '0x0x00000011')
-    assert buffer.read(5) == '0x0x00000011'
+    assert buffer.read(5) == '0x00000000'
+    buffer.write('W', 5, '0x00000011')
+    assert buffer.read(5) == '0x00000011'
