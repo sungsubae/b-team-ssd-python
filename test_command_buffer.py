f48fce36863c93f7606e1f8b008c4cb20348df30
lkwoo <dlrmsdn135@naver.com>
2025-07-16 13:58:16 +0900

    add buffer test
    add buffer test in 명세서, refactoring

--- test_command_buffer.py
+++ test_command_buffer.py
@@ -4,60 +4,77 @@ from unittest.mock import call, patch, mock_open
 import builtins
 import os
 from shell import Shell, main
-
-buffer_path = r'.\buffer'
-
-
-def write_buffer(index: int, command: str):
-    """
-    index에 해당하는 buffer를 작성한다.
-    :param index: 1~5
-    :param command: ex) R_10, W_10_0x12345678
-    """
-    if not os.path.exists(buffer_path):
-        os.makedirs(buffer_path)
-
-    for filename in os.listdir(buffer_path):
-        if filename.startswith(f'{index}_'):
-            file_path = os.path.join(buffer_path, filename)
-            os.remove(file_path)
-
-    with open(os.path.join(buffer_path, f"{index}_{command}"), 'w') as f:
-        pass
+from buffer import Buffer
+from ssd import SSD
 
 
-@pytest.fixture
-def init_buffer():
-    for i in range(1, 6):
-        write_buffer(i, 'empty')
+def test_command_buffer_flush():
+    buffer = Buffer()
+    buffer.make_init_buffer()
+    buffer.write('W', 1, '0x00001111')
+    buffer.write('W', 2, '0x22220000')
+    buffer.write('W', 3, '0x00003333')
+    buffer.write('W', 4, '0x44440000')
+    buffer.write('W', 5, '0x00005555')
 
-
-def test_command_buffer_flush(capsys, mocker: MockerFixture, init_buffer):
-    # write_buffer(1, 'R_10')
-    # write_buffer(2, 'W_10_0x12341234')
-    # do flush here
+    buffer.flush()
 
     expected_files = {'1_empty', '2_empty', '3_empty', '4_empty', '5_empty'}
-    actual_files = set(os.listdir(buffer_path))
+    actual_files = set(os.listdir(r"./buffer"))
 
     assert expected_files.issubset(actual_files)
 
 
 # 1_W_20_0xABCDABCD , 2_W_20_0x12341234 , 3_E_20_1
-def test_command_buffer_example_1(mocker: MockerFixture, init_buffer):
+def test_command_buffer_example_1(mocker: MockerFixture):
     shell_write_mock = mocker.patch('shell.Shell._write')
     shell_erase_mock = mocker.patch('shell.Shell._erase')
 
-    shell = Shell()
-    # command buffer 적용되면 아래 주석 해제 후 TEST
-    # shell.write(20, "0xABCDABCD")
-    # shell.write(20, "0x12341234")
-    # shell.erase(20, 1)
+    buffer = Buffer()
+    buffer.make_init_buffer()
+    buffer.write('W', 20, "0xABCDABCD")
+    buffer.write('W', 20, "0x12341234")
+    buffer.erase(20, 1)
+
+    expected_files = {'1_E_20_1', '2_empty', '3_empty', '4_empty', '5_empty'}
+    actual_files = set(os.listdir(r"./buffer"))
 
     shell_write_mock.assert_not_called()
     shell_erase_mock.assert_not_called()
-
-    expected_files = {'1_E_20_1', '2_empty', '3_empty', '4_empty', '5_empty'}
-    actual_files = set(os.listdir(buffer_path))
-
     assert expected_files.issubset(actual_files)
+
+
+def test_command_buffer_example_2(mocker: MockerFixture):
+    shell_read_mock = mocker.patch('shell.Shell._read')
+
+    buffer = Buffer()
+    buffer.make_init_buffer()
+    buffer.write('W', 50, "0xAAAABBBB")
+    buffer.write('W', 20, "0xABABCCCC")
+    buffer.read(50)
+
+    expected_buffer_set_without_index = {'W_50_0xAAAABBBB', 'W_20_0xABABCCCC', 'empty'}
+    actual_files = set([fn[2:] for fn in os.listdir(r"./buffer")])
+
+    shell_read_mock.assert_not_called()
+
+    assert expected_buffer_set_without_index == actual_files
+    assert SSD().read_output().strip() == '0xAAAABBBB'
+
+
+def test_command_buffer_example_3(mocker: MockerFixture):
+    shell_write_mock = mocker.patch('shell.Shell._write')
+    shell_erase_mock = mocker.patch('shell.Shell._erase')
+
+    buffer = Buffer()
+    buffer.make_init_buffer()
+    buffer.write('W', 20, "0xABCDABCD")
+    buffer.erase(10, 4)
+    buffer.erase(12, 3)
+
+    expected_buffer_set_without_index = {'W_20_0xABCDABCD', 'E_10_5', 'empty'}
+    actual_files = set([fn[2:] for fn in os.listdir(r"./buffer")])
+
+    shell_write_mock.assert_not_called()
+    shell_erase_mock.assert_not_called()
+    assert expected_buffer_set_without_index == actual_files
